8b9e0fc7b2b1cf3573e7a5ee9b341e4c7816c2a0
Henrique Silva Leão <henriqueleao040522@outlook.com>
2025-09-03 14:37:57 -0300

    feat: Adiciona funcionalidades de remover e concluir tarefas

--- script.js
+++ script.js
@@ -10,9 +10,20 @@ let tarefas = tarefassalvas ? JSON.parse(tarefassalvas) : [];
 function renderizarTarefas() {
   
     let htmltext = '';
+    let cont = 0
 
     for (const tarefa of tarefas) {
-    htmltext += `<li>${tarefa}</li>`;
+
+        const classeCSS = tarefa.concluida ? 'tarefa-concluida' : '';
+        htmltext += 
+                `<div class="tarefa-item-container">
+                   <li class="${classeCSS}" id="tarefa-${cont}" data-index="${cont}">
+                      <span>${tarefa.texto}</span> 
+                   </li>
+                   <button class="botao-remover" data-index="${cont}">X</button> 
+                </div>`;
+       cont ++
+       console.log(cont)
     }
 
    lista_taferas.innerHTML = htmltext;
@@ -20,11 +31,16 @@ function renderizarTarefas() {
 };
 
 
-
 btnadd.onclick = function()
 {
     tarefa = tarefasinput.value
-    tarefas.push(tarefa)
+
+    const novaTarefaObjeto = {
+    texto: tarefa,
+    concluida: false
+    };
+
+    tarefas.push(novaTarefaObjeto)
 
     const tarefasEmString = JSON.stringify(tarefas);
 
@@ -37,6 +53,29 @@ btnadd.onclick = function()
     renderizarTarefas();
 
 };
+
+lista_taferas.addEventListener('click', function(event) {
+    
+    const elementoClicado = event.target;
+    
+    if (elementoClicado.classList.contains('botao-remover')) {
+        const indice = parseInt(elementoClicado.dataset.index);
+        
+        tarefas.splice(indice, 1);
+    }
+    else if (elementoClicado.closest('li')) {
+        const liClicado = elementoClicado.closest('li');
+        const indice = parseInt(liClicado.dataset.index);
+
+        tarefas[indice].concluida = !tarefas[indice].concluida;
+    }
+
+    localStorage.setItem('minhaListaDeTarefas', JSON.stringify(tarefas));
+    renderizarTarefas();
+});
+
+
+
 renderizarTarefas();
 
 
